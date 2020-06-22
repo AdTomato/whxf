03bafe2dfcdde4ead4ce141f2056db30e02fee3c
xiaodouzi <1950746552@qq.com>
2020-06-22 13:13:15 +0800

    1. 解决前端跨域问题

--- src/main/java/com/authine/cloudpivot/web/api/WebApiBootStartupApplication.java
+++ src/main/java/com/authine/cloudpivot/web/api/WebApiBootStartupApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -28,5 +33,27 @@ public class WebApiBootStartupApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(WebApiBootStartupApplication.class);
+    }
+
+    /**
+     * 跨域配置
+     *
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+
     }
 }
