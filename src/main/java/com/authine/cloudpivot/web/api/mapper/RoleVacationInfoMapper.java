8dd99a4ca4dc3e9b9ae76a8554de2035983b616a
weiyao <2386409261@qq.com>
2020-07-07 10:51:35 +0800

    1. 增加消防员和专职消防员请假信息

--- src/main/java/com/authine/cloudpivot/web/api/mapper/RoleVacationInfoMapper.java
+++ src/main/java/com/authine/cloudpivot/web/api/mapper/RoleVacationInfoMapper.java
@@ -15,14 +15,26 @@ public interface RoleVacationInfoMapper {
     //weiyao 获取钉钉角色Id
     String getddRoleId(String name);
 
-    //批量插入请假人员信息
+    //批量插入请假人员信息-干部
     Integer insertVacationDetailList(List<RoleVacationInfo> infolist);
+    //批量插入请假人员信息-消防员
+    Integer insertXfyVacationDetailList(List<RoleVacationInfo> infolist);
+    //批量插入请假人员信息-专职消防员
+    Integer insertZzxfyVacationDetailList(List<RoleVacationInfo> infolist);
 
-    //插入请假信息主表
+    //插入请假信息主表-干部
     Integer insertRoleVacation(RoleVacationInfo info);
+    //插入请假信息主表-消防员
+    Integer insertXfyRoleVacation(RoleVacationInfo info);
+    //插入请假信息主表-专职消防员
+    Integer insertZzxfyRoleVacation(RoleVacationInfo info);
 
     //查询是否有今天数据
     Integer getTodayCount();
+    //查询是否有今天数据-消防员
+    Integer getXfyTodayCount();
+    //查询是否有今天数据-专职消防员
+    Integer getZzxfyTodayCount();
 
     //根据钉钉部门id获取部门名称
     String getDeptNameByDDdeptId(String sourceId);
